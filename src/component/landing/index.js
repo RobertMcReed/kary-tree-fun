870ecda299d79b5c32118dbb33da9f28f49525b0
Robert Reed <robert.mc.reed@gmail.com>
2018-04-11 22:16:05 -0700

    added simple form and Kary to landing

--- src/component/landing/index.js
+++ src/component/landing/index.js
@@ -1,11 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+
+import Kary from '../../lib/kary';
+import SimpleForm from '../simple-form';
 
 class Landing extends Component {
+  state = {
+    tree: new Kary('hey there bub'),
+  }
+
   render() {
     return (
-      <h2>
-        Hello there bub
-      </h2>
+      <Fragment>
+        <pre>
+          {JSON.stringify(this.state.tree, null, 4)}
+        </pre>
+        <SimpleForm 
+          buttonStyle={{ color: 'black' }} 
+          inputStyle={{ color: 'purple', fontSize: '1em' }}
+          placeholder="Hey there bubba..."
+          buttonText="Add Node"
+          onSubmit={this.addNode}
+        />
+      </Fragment>
     );
   }
 }
